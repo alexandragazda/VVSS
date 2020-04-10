abaa04cf65b4b30deccfd67abc3ec109d194e9e9
Alexandra Gazda <alexandragazda@yahoo.com>
2020-04-10 14:05:30 +0300

    lab3 done

--- PizzaShop/src/test/java/pizzashop/service/PizzaServiceTest_WBT.java
+++ PizzaShop/src/test/java/pizzashop/service/PizzaServiceTest_WBT.java
@@ -17,20 +17,26 @@ class PizzaServiceTest_WBT {
 
     private static MenuRepository menuRepository;
     private static PaymentRepository paymentRepository;
-    private static PaymentRepository paymentRepositoryEmptyFile;
-    private static PizzaService pizzaServiceEmptyFile;
     private static PizzaService pizzaService;
+
     private static final String SERVICEEXEPTIONMSG="Type has to be specified!";
-    private static final Double totalAmountCard = 50.5;
-    private static final Double totalAmountCash = 130.0;
 
     @BeforeAll
     static void init(){
         BasicConfigurator.configure();
         menuRepository= new MenuRepository();
+        paymentRepository= new PaymentRepository("data/paymentsTest-WBT");
+        pizzaService= new PizzaService(menuRepository,paymentRepository);
+    }
 
-        paymentRepositoryEmptyFile= new PaymentRepository("data/paymentsTestEmpty-WBT");
-        pizzaServiceEmptyFile= new PizzaService(menuRepository,paymentRepositoryEmptyFile);
+
+    @AfterEach
+    void end(){
+        try(BufferedWriter bw=new BufferedWriter(new FileWriter("target/test-classes/data/paymentsTest-WBT"))){
+            bw.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         paymentRepository= new PaymentRepository("data/paymentsTest-WBT");
         pizzaService= new PizzaService(menuRepository,paymentRepository);
@@ -40,7 +46,10 @@ class PizzaServiceTest_WBT {
     @DisplayName("WBT-non-valid")
     void test1() {
         try {
-           pizzaService.getTotalAmount(null);
+            pizzaService.addPayment(4,PaymentType.Card,10.0);
+            assertTrue(pizzaService.getPayments().size() == 1);
+            assertTrue(pizzaService.getPayments().get(0).getType() == PaymentType.Card);
+            pizzaService.getTotalAmount(null);
         } catch (ServiceException ex) {
             assertTrue(ex.getMessage().equals(SERVICEEXEPTIONMSG));
         }
@@ -49,14 +58,33 @@ class PizzaServiceTest_WBT {
     @Test
     @DisplayName("WBT-valid1")
     void test2() {
-        assertTrue(pizzaServiceEmptyFile.getPayments().isEmpty());
-        assertTrue(pizzaServiceEmptyFile.getTotalAmount(PaymentType.Card) == 0.0F);
+        assertTrue(pizzaService.getPayments().isEmpty());
+        assertTrue(pizzaService.getTotalAmount(PaymentType.Card) == 0.0F);
     }
 
     @Test
     @DisplayName("WBT-valid2")
     void test3() {
-        assertTrue(pizzaService.getTotalAmount(PaymentType.Card) == totalAmountCard);
-        assertTrue(pizzaService.getTotalAmount(PaymentType.Cash) == totalAmountCash);
+        pizzaService.addPayment(3,PaymentType.Card,10.0);
+        pizzaService.addPayment(2,PaymentType.Card,13.5);
+        pizzaService.addPayment(8,PaymentType.Card,20.0);
+
+        assertTrue(pizzaService.getPayments().size() == 3);
+        assertTrue(pizzaService.getPayments().get(1).getType() == PaymentType.Card);
+
+        assertTrue(pizzaService.getTotalAmount(PaymentType.Cash) == 0.0F);
+    }
+
+    @Test
+    @DisplayName("WBT-valid3")
+    void test4() {
+        pizzaService.addPayment(3,PaymentType.Cash,10.0);
+        pizzaService.addPayment(2,PaymentType.Card,13.5);
+        pizzaService.addPayment(8,PaymentType.Cash,20.0);
+
+        assertTrue(pizzaService.getPayments().size() == 3);
+        assertTrue(pizzaService.getPayments().get(1).getType() == PaymentType.Card);
+
+        assertTrue(pizzaService.getTotalAmount(PaymentType.Card) == 13.5);
     }
 }
